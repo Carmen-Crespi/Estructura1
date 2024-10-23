cbec4eefa1db7e5ef03b392284304a8422123b36
Carmen-Crespi <ccrespi.5727@unimar.edu.ve>
2024-10-22 21:31:50 -0400

    Ejercicio3

--- src/estructurarosa/EstructuraRosa.java
+++ src/estructurarosa/EstructuraRosa.java
@@ -1,21 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package estructurarosa;
 
-/**
- *
- * @author Carmen Crespi
- */
 public class EstructuraRosa {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        ejercicio3(); }
+                
+       public  static void ejercicio3(){
+
+        int [][] Matriz= new int[3][3];
+        int difNum = 0; // Cantidad de numeros diferentes
+
+        Matriz[0][0]=1;
+        Matriz[0][1]=0;
+        Matriz[0][2]=0;
+
+        Matriz[1][0]=0;
+        Matriz[1][1]=1;
+        Matriz[1][2]=0;
+
+        Matriz[2][0]=0;
+        Matriz[2][1]=0;
+        Matriz[2][2]=1;
+
+
+        for (int i = 0; i < Matriz.length; i++) {  
+            for (int j = 0; j <Matriz.length ; j++) {
+
+                if (i == j) { // Revisa la diagonal
+                    if (Matriz[i][j]!=1) {
+                        difNum++; // Si en la diagonal es diferente de 1 suma un numero diferente 
+                    }
+                } else{ 
+                    if (Matriz[i][j]!=0) { //Si es diferente de cero suma un error
+                        difNum++; 
+                    }
+                }
+
+            }
+        }
+
+        if (difNum<=0) { //Si hay 0 errores es identidad
+            System.out.println("Tu matriz es identidad");
+        } else {
+            System.out.println("Tu matriz no es identidad");
+        }
+
     }
     
-}
+        
+        
+}  
